d9324ee0d85c5ce8b85d66204a946d71a928ce41
audasviator <prieran@ya.ru>
2016-08-02 01:02:54 +0300

    Не помню, что я накодил

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentAdapter.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentAdapter.java
@@ -1,36 +1,69 @@
 package ru.yandex.yamblz.ui.fragments;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.yandex.yamblz.R;
 
 class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ContentHolder> {
 
     private final Random rnd = new Random();
     private final List<Integer> colors = new ArrayList<>();
+    private boolean mIsAmazingStyle;
 
     @Override
     public ContentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ContentHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.content_item, parent, false));
+        return new ContentHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.content_item, parent, false), parent.getContext().getResources().getDrawable(R.drawable.ramko));
     }
 
     @Override
     public void onBindViewHolder(ContentHolder holder, int position) {
-        holder.bind(createColorForPosition(position));
+
+        holder.bind(createColorForPosition(position), mIsAmazingStyle);
     }
 
     @Override
     public int getItemCount() {
         return Integer.MAX_VALUE;
+    }
+
+    public void deleteItem(int position) {
+        colors.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void swapItems(int fromPosition, int toPosition) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(colors, i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(colors, i, i - 1);
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
+    public void setAmazingStyle(boolean isAmazingStyle) {
+        mIsAmazingStyle = isAmazingStyle;
+    }
+
+    public boolean getAmazingStyle() {
+        return mIsAmazingStyle;
     }
 
     private Integer createColorForPosition(int position) {
@@ -41,13 +74,29 @@ class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ContentHolder> 
     }
 
     static class ContentHolder extends RecyclerView.ViewHolder {
-        ContentHolder(View itemView) {
+        private Drawable mBackgroundDrawable;
+
+        @BindView(R.id.content_item_text_view)
+        TextView mTextView;
+
+        @BindView(R.id.content_item_image_view)
+        ImageView mImageView;
+
+        ContentHolder(View itemView, Drawable background) {
             super(itemView);
+            mBackgroundDrawable = background;
+            ButterKnife.bind(this, itemView);
         }
 
-        void bind(Integer color) {
-            itemView.setBackgroundColor(color);
-            ((TextView) itemView).setText("#".concat(Integer.toHexString(color).substring(2)));
+        void bind(Integer color, boolean amazingStyle) {
+            // Через ItemDecoration делать некруто, при перетаскивании рамка останется на месте
+            if (amazingStyle && getAdapterPosition() % 2 == 0) {
+                mImageView.setImageDrawable(mBackgroundDrawable);
+            } else {
+                mImageView.setImageDrawable(null);
+            }
+            mTextView.setBackgroundColor(color);
+            mTextView.setText("#".concat(Integer.toHexString(color).substring(2)));
         }
     }
 }
--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -1,21 +1,33 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import ru.yandex.yamblz.R;
 
 public class ContentFragment extends BaseFragment {
 
     @BindView(R.id.rv)
     RecyclerView rv;
+
+    @BindView(R.id.fragment_content_plus_button)
+    Button buttonPlus;
 
     @NonNull
     @Override
@@ -26,7 +38,135 @@ public class ContentFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
         rv.setAdapter(new ContentAdapter());
+        //rv.addItemDecoration(new amazingDividerItemDecoration(getContext()));
+
+        ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                ContentAdapter adapter = (ContentAdapter) rv.getAdapter();
+                int endAdapterPosition = target.getAdapterPosition();
+                int startAdapterPosition = viewHolder.getAdapterPosition();
+                adapter.swapItems(startAdapterPosition, endAdapterPosition);
+                //adapter.notifyItemChanged(startAdapterPosition);
+                //adapter.notifyItemChanged(endAdapterPosition);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                ContentAdapter adapter = (ContentAdapter) rv.getAdapter();
+                adapter.deleteItem(position);
+                updateRv();
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                int intX = (int) Math.sqrt(dX * dX + dY * dY) / 2 % 255;
+                c.drawRGB(255, 255 - intX, 255 - intX);
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(rv);
+    }
+
+    @OnClick(R.id.fragment_content_plus_button)
+    void plusSpan() {
+        updateSpanCount(true, rv);
+    }
+
+    @OnClick(R.id.fragment_content_minus_button)
+    void minusSpan() {
+        updateSpanCount(false, rv);
+    }
+
+    @OnClick(R.id.fragment_content_style_button)
+    void changeStyle() {
+        ContentAdapter contentAdapter = (ContentAdapter) rv.getAdapter();
+        contentAdapter.setAmazingStyle(!contentAdapter.getAmazingStyle());
+
+        updateRv();
+    }
+
+    private void updateRv() {
+        // Некрасиво
+        ContentAdapter contentAdapter = (ContentAdapter) rv.getAdapter();
+        GridLayoutManager gridLayoutManager = (GridLayoutManager) rv.getLayoutManager();
+        int spanCount = gridLayoutManager.getSpanCount();
+        int first = gridLayoutManager.findFirstVisibleItemPosition();
+        int last = gridLayoutManager.findLastVisibleItemPosition();
+        if (last + spanCount < contentAdapter.getItemCount()) {
+            contentAdapter.notifyItemRangeChanged(first, last + spanCount);
+        } else {
+            contentAdapter.notifyItemRangeChanged(first, last);
+        }
+    }
+
+    private void updateSpanCount(boolean increase, RecyclerView recyclerView) {
+        try {
+            GridLayoutManager gridLayoutManager = (GridLayoutManager) recyclerView.getLayoutManager();
+            int spanCount = gridLayoutManager.getSpanCount();
+
+            if (increase) {
+                spanCount++;
+            } else {
+                if (spanCount > 1) {
+                    spanCount--;
+                } else {
+                    Toast.makeText(getContext(), "Не надо", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            gridLayoutManager.setSpanCount(spanCount);
+
+            // Идея нагло украдена у @guliash (Artem Gilmudinov)
+            // Хотя в презентации написано, что эти методы с анимацией, но ктож её смотрит?
+            // Эксперименты показали, что можно вызвать любой метод notifyXyzChanged
+            // Те элементы, которые сейчас показываются на экране анимированно сдвигаются
+            // При скроллинге новые элементы появляются сразу в нужном виде
+            // Пробежавшись отладчиком, можно увидеть, как rv что-то там обновляет и анимирует =)
+            rv.getAdapter().notifyItemChanged(0);
+        } catch (ClassCastException e) {
+            Toast.makeText(getContext(), "Не grid", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class amazingDividerItemDecoration extends RecyclerView.ItemDecoration {
+        private Drawable mDivider;
+
+        public amazingDividerItemDecoration(Context context) {
+            final TypedArray a = context.obtainStyledAttributes(new int[]{android.R.attr.listDivider});
+            mDivider = a.getDrawable(0);
+            a.recycle();
+        }
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getTop() + params.topMargin;
+                int bottom = child.getBottom() + params.bottomMargin;
+
+                int left = child.getLeft() + params.leftMargin;
+                int right = child.getRight() + params.rightMargin;
+
+                mDivider.setBounds(left, top, right, bottom);
+                //mDivider.draw(c);
+                Paint paint = new Paint();
+                paint.setStrokeWidth(10f);
+                c.drawLine(left, top, right, top, paint);
+                c.drawLine(left, top, left, bottom, paint);
+                c.drawLine(left, top, left, bottom, paint);
+                c.drawLine(right, top, right, bottom, paint);
+            }
+        }
     }
 }
